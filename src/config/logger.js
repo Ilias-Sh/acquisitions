64e801136043bc8ee0a8a4dc1406888a92228678
Ilias <iliassahli126@gmail.com>
2026-02-28 04:56:40 +0000

    Implement Auth

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,13 +1,13 @@
-import { winston } from 'winston'; 
+import winston from 'winston'; 
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.combine((
+  format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
     winston.format.json()
-  )),
-  defaultMeta: { service: 'acquisition-api' },
+  ),
+  defaultMeta: { service: 'acquisitions-api' },
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
@@ -18,8 +18,8 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
+      winston.format.colorize(),
+      winston.format.simple(),
     )
   }));
 }
